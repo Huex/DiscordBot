1894d737eb871f45e0d60b71c0367cc71c3bc3e3
Huex <tohuex@gmail.com>
2018-07-09 12:50:37 +0300

    add logs entitys

---
+++ DiscordBot/Log/LogEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    public abstract class LogEntity
+    {
+        public event Func<LogMessage, Task> Log;
+
+        protected void RaiseLog(LogSeverity severity, string message, Exception exception = null)
+        {
+            RaiseLog(new LogMessage(severity, GetType().Name, message, exception));
+        }
+
+        protected void RaiseLog(LogMessage message)
+        {
+            Log?.Invoke(message);
+        }
+
+        protected async Task RaiseLogAsync(LogMessage message)
+        {
+            await Log?.Invoke(message);
+        }
+
+        protected async Task RaiseLogAsync(Discord.LogMessage message)
+        {
+            await Log?.Invoke(new LogMessage(message));
+        }
+    }
+}
---
+++ DiscordBot/Log/LogMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DiscordBot
+{
+    public struct LogMessage
+    {
+        public LogMessage(Discord.LogMessage logMessage)
+        {
+            Severity = (LogSeverity)logMessage.Severity;
+            Source = logMessage.Source;
+            Message = logMessage.Message;
+            Exception = logMessage.Exception;
+        }
+
+        public LogMessage(LogSeverity severity, string source, string message, Exception exception = null)
+        {
+            Severity = severity;
+            Source = source;
+            Message = message;
+            Exception = exception;
+        }
+
+        public LogSeverity Severity { get; }
+        public string Source { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
