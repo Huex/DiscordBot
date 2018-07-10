54f14428ef408493293df8546b0ff0117b65d6e6
Huex <tohuex@gmail.com>
2018-07-10 13:53:31 +0300

    add experemental data managers

---
+++ DiscordBot/IDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot
+{
+    public interface IDataProvider
+    {
+
+        void SetGuildConfig(CommandConfig guildConfig);
+
+        CommandConfig GetGuildConfig(ulong guildId);
+    }
+}
