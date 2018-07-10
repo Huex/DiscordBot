e2fb62ebfedd045f7869d0c23b0b13a18fd5e736
Huex <tohuex@gmail.com>
2018-07-10 13:30:30 +0300

    add NotifyDictonary

---
+++ DiscordBot/Utils/NotifyDictonary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Utils
+{
+    public class NotifyDictonary<Tkey, Tvalue> : Dictionary<Tkey, Tvalue>
+    {
+        public NotifyDictonary() : base() { }
+        NotifyDictonary(int capacity) : base(capacity) { }
+
+        public event Action<Tkey> ValueUpdated;
+
+        private void OnValueUpdated
+            (Tkey key)
+        {
+            ValueUpdated?.Invoke(key);
+        }
+
+        public new Tvalue this[Tkey key]
+        {
+            get
+            {
+                return base[key];
+            }
+            set
+            {
+                base[key] = value;
+                OnValueUpdated(key);
+            }
+        }
+    }
+}
