0b015f0f1efeea4bbb05b91e1aa3983ff26bb000
Huex <tohuex@gmail.com>
2018-07-11 10:30:21 +0300

    add removal of unconnected command modules

--- DiscordBot/CommandHandler.cs
+++ DiscordBot/CommandHandler.cs
@@ -2,7 +2,9 @@
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace DiscordBot.Core
@@ -24,7 +26,27 @@ namespace DiscordBot.Core
             {
                 _config = value;
                 ConfigUpdated?.Invoke(_config.Id, _config);
+                UpdateCommandsAsync(Config).ConfigureAwait(true);
             }
+        }
+
+        private async Task UpdateCommandsAsync(CommandConfig config)
+        {
+            foreach (var module in Commands.Modules)
+            {
+                var needed = new List<string>(config.Modules);
+                if (!needed.Contains(module.Name))
+                {
+                    await Commands.RemoveModuleAsync(module);
+                }
+            }
+        }
+
+        private static string GetNameFromModule(Type module)
+        {
+            var attributes = new List<CustomAttributeData>(module.GetCustomAttributesData());
+            var needed = attributes?.Find(p => p.AttributeType == typeof(NameAttribute));
+            return (string)needed?.ConstructorArguments[0].Value;
         }
 
         public event Action<ulong, CommandConfig> ConfigUpdated;
@@ -65,7 +87,7 @@ namespace DiscordBot.Core
                 await message.Channel.SendMessageAsync(result.ErrorReason);
                 await Task.Delay(500);
                 await message.AddReactionAsync(new Discord.Emoji("⁉"));
-                
+
             }
             else
             {
@@ -74,96 +96,4 @@ namespace DiscordBot.Core
             }
         }
     }
-
-    //public class GuildWorker : BotServiceBase
-    //{
-    //    public GuildConfig Config => _data.Config;
-
-    //    private ServiceCollection _map;
-
-    //    private readonly DiscordSocketClient _discord;
-    //    private readonly CommandService _commands;
-    //    private readonly GuildDataManager _data;
-    //    private readonly Collection<Type> _availableModules;
-
-    //    public GuildWorker(DiscordSocketClient discord, ServiceCollection map, Collection<Type> modules, GuildConfig config)
-    //    {
-    //        _discord = discord;
-    //        _availableModules = modules;
-    //        _commands = new CommandService();
-    //        _commands.Log += (p) =>
-    //        {
-    //            RaiseLog(new LogMessage(p));
-    //            return Task.CompletedTask;
-    //        };
-    //        _data = new GuildDataManager(config);
-    //        _data.Log += (p) =>
-    //        {
-    //            RaiseLog(p);
-    //            return Task.CompletedTask;
-    //        };
-    //        _map = map;
-    //        _map.AddSingleton(_data);
-    //        _map.AddSingleton(_commands);
-    //    }
-
-    //    public void SyncConfig()
-    //    {
-    //        _data.SyncConfigWithLocalFile();
-    //        UpdateModulesByConfig();
-    //    }
-
-    //    public void UpdateModulesByConfig()
-    //    {
-    //        foreach (var moduleType in _availableModules)
-    //        {
-    //            if (moduleType.IsSubclassOf(typeof(ModuleBase)))
-    //            {
-    //                var neededAttributes = new List<CustomAttributeData>(moduleType.GetCustomAttributesData());
-    //                var need = neededAttributes?.Find(x => x.Constructor.DeclaringType == typeof(NameAttribute));
-    //                var value = need?.ConstructorArguments[0].Value;
-    //                if (value != null)
-    //                {
-    //                    if (_data.Config.Modules.Contains((string)value))
-    //                    {
-    //                        _commands.AddModuleAsync(moduleType);
-    //                    }
-    //                }
-    //            }
-    //        }
-    //        _map.Remove(_map.FirstOrDefault(d => d.ServiceType == _commands.GetType()));
-    //        _map.AddSingleton(_commands);
-    //    }
-
-    //    public async Task HandleMessage(SocketMessage message)
-    //    {
-    //        await HandleCommand(message as SocketUserMessage);
-    //    }
-
-    //    private async Task HandleCommand(SocketUserMessage msg)
-    //    {
-    //        if (msg == null)
-    //        {
-    //            return;
-    //        }
-    //        var prefix = _data.Config.Prefix;
-    //        int prefixInt = prefix.Length - 1;
-    //        if (msg.HasStringPrefix(prefix, ref prefixInt) || msg.HasMentionPrefix(_discord.CurrentUser, ref prefixInt))
-    //        {
-    //            await ProcessCommandAsync(prefixInt, msg);
-    //        }
-    //    }
-
-    //    private async Task ProcessCommandAsync(int prefixInt, SocketUserMessage message)
-    //    {
-    //        var context = new SocketCommandContext(_discord, message);
-
-    //        await message.Channel.TriggerTypingAsync();
-    //        var result = await _commands.ExecuteAsync(context, prefixInt, _map.BuildServiceProvider());
-    //        if (!result.IsSuccess)
-    //        {
-    //            await message.Channel.SendMessageAsync(result.ErrorReason);
-    //        }
-    //    }
-    //}
 }
