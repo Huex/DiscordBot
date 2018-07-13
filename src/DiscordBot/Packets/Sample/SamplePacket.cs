5d01a69ed625956f74485c35a01f4362273fb722
Huex <tohuex@gmail.com>
2018-07-13 12:32:42 +0300

    add example to SamplePacket

--- src/DiscordBot/Packets/Sample/SamplePacket.cs
+++ src/DiscordBot/Packets/Sample/SamplePacket.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DiscordBot.Packets.Sample
 {
@@ -15,6 +16,19 @@ namespace DiscordBot.Packets.Sample
             Services.AddSingleton(myService);              // если myService нужно как-то конфигурировать, то делай это в этом классе, 
             GuildModules.Add(typeof(SampleModule));        // или монжно еще выше по архитектурке, т.е. сюда в конструкторе передавать чото :^)
             DMModules.Add(typeof(SampleModule));
+
+            DiscordInitialized += SubscribeOnDiscordEvents;
+        }
+
+        private Task Discord_Ready()
+        {
+            RaiseLog(new LogMessage(LogSeverity.Critical, "Я ИЗ ЧЕЧНИ", "ДАРОВА))"));
+            return Task.CompletedTask;
+        }
+
+        private void SubscribeOnDiscordEvents()
+        {
+            Discord.Ready += Discord_Ready;
         }
     }
 }
