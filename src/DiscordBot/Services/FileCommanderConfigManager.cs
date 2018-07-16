ee55c9e8ee26bb1b5bbc5cf54710ee3a8510d12e
Huex <tohuex@gmail.com>
2018-07-16 12:06:32 +0300

    #5 add template of config data manager

---
+++ src/DiscordBot/Services/FileCommanderConfigManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.Services
+{
+    public class FileCommanderConfigManager : FileManager, ICommandConfigsProvider
+    {
+        private readonly Dictionary<ulong, CommandConfig> _commandConfigs = new Dictionary<ulong, CommandConfig>();
+
+        private string KatalogPath => SERVICE_KATALOGNAME + PATH_SEPARATOR + COMMANDCONFIGS_KATALOGNAME;
+
+        public const string COMMANDCONFIGS_KATALOGNAME = "commands";
+
+        public IReadOnlyDictionary<ulong, CommandConfig> CommandConfigs => _commandConfigs;
+
+        public void AddCommandConfig(CommandConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommandConfig(ulong id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommandConfig(ulong id, CommandConfig config)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommandConfig(ulong id, string prefix, IEnumerable<string> modules)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DiscordBot/Services/FileManager.cs
@@ -0,0 +1,29 @@
+﻿using DiscordBot.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DiscordBot.Services
+{
+    public class FileManager
+    {
+        public static readonly Encoding TextEncoding = Encoding.UTF8;
+
+        public const string FILE_TYPE = "json";
+        public const string SERVICE_KATALOGNAME = "data";
+        public const string PATH_SEPARATOR = "/";
+        public const string FILE_TYPE_SEPARATOR = ".";
+
+        protected static T ReadJson<T>(string path)
+        {
+            return (T)JsonConvert.DeserializeObject(File.ReadAllText(path, TextEncoding), typeof(T));
+        }
+
+        protected static void WriteJson<T>(string path, T obj)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(obj, new JsonSerializerSettings { ContractResolver = new IgnoreParentPropertiesResolver(true) }), TextEncoding);
+        }
+    }
+}
